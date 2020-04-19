970eb3eea707f13ee0a14e2973b27d206dfcfc62
jeremiastome <jeremias.tome1@gamil.com>
2020-04-19 14:58:58 -0300

    Home de la aplicación

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Cursos from './componentes/Cursos';
+
+function App() {
+  return (
+    <div className="App">
+      <Cursos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Cursos.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Cursos = props => {
+
+    return(
+        <div>
+      Lista de cursos
+    </div>
+    )
+}
+  
+export default Cursos;
